a042e685a1c035c155c62f0cb397e4f3f8fcb9b3
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-05 09:58:53 +0200

    CHANGE : class JUnite passee en v4

---
+++ testing/fr/eni/testing/CalculatriceTest.java
@@ -0,0 +1,14 @@
+package fr.eni.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatriceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
