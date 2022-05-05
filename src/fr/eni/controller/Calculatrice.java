9a7505513ae01f2c8a7f9cd1b8939b5ebb8f36f2
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-05 09:45:26 +0200

    ADD : Creation repo

---
+++ src/fr/eni/controller/Calculatrice.java
@@ -0,0 +1,12 @@
+package fr.eni.controller;
+
+public class Calculatrice {
+
+	public int addition(int a, int b) {
+		return a+b;
+	}
+	
+	public double division(int a, int b) {
+		return a/b;
+	}
+}
---
+++ src/fr/eni/testing/Test_1.java
@@ -0,0 +1,14 @@
+package fr.eni.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
